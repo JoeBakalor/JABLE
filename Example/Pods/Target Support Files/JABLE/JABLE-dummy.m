7b8ad29c98e1a1a5d273ea7118e3f1aeb9395294
Joe Bakalor <jbak688@Joes-MacBook-Pro.local>
2018-03-01 14:18:54 -0600

    Create base pod

---
+++ Example/Pods/Target Support Files/JABLE/JABLE-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JABLE : NSObject
+@end
+@implementation PodsDummy_JABLE
+@end
---
+++ Example/Pods/Target Support Files/Pods-JABLE_Example/Pods-JABLE_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JABLE_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JABLE_Example
+@end
